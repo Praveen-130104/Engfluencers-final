54013629404bc885f40bdf2ef703fe2cd4487ad8
Praveen-130104 <727721euec113@skcet.ac.in>
2023-11-30 22:56:20 +0530

    cors 3

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ app.use("/admin", router);
 app.use("/user", userRouter);
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
