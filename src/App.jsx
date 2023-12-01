7b01a4006089740e047a38400b9019b66c4fb1aa
Praveen-130104 <727721euec113@skcet.ac.in>
2023-12-01 07:35:13 +0530

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import 'animate.css';
 import { BrowserRouter as Router , Routes , Route } from 'react-router-dom'
 import AdminLogin from './components/AdminLogin';
-import AdminFileUpload from './components/AdminFileUpload/AdminFileUpload';
+import AdminFileUpload from './components/adminFileUpload/AdminFileUpload';
 import AdminNavbar from './components/adminFileUpload/AdminNavbar';
 import AdminResourceUpload from './components/adminFileUpload/AdminResourceUpload';
 
